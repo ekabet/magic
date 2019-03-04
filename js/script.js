82e414594b7fa7fd51ed960c22912016585654be
Alexey197 <margadonnn3@gmail.com>
2019-03-04 16:08:53 +0300

    Вторая лекция

--- js/script.js
+++ js/script.js
@@ -18,8 +18,17 @@ var getWizardY = function (height) {
     return height  * 2 / 3;
 };
 
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var gradient = ctx.createLinearGradient(0, 0, 300, 150);
 
 
+gradient.addColorStop(0, 'green');
+gradient.addColorStop(1, 'blue');
+ctx.fillStyle = gradient;
+ctx.fillRect(0, 0, 300, 150);
+ctx.strokeStyle = 'yellow';
+ctx.strokeRect(0, 0, 300, 150);
 
 
 
